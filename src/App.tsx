133680bbe99cb17968279df85d8e3317fa9df6a4
rave1 <jakubsopel7@gmail.com>
2021-10-29 17:41:03 +0200

    feat: set start and finish

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,37 @@ const Container = styled.div`
 
 interface WrapperProps {
   isWall: boolean;
-  isStart?: boolean;
-  isFinish?: boolean;
+  isStart: boolean;
+  isFinish: boolean;
+  isVisited: boolean;
 }
 
 const Wrapper = styled.div<WrapperProps>`
   border: 1px solid black;
-  background-color: ${(props) => props.isWall ? '#f5f' : '#fff'};
-`
+  background-color: ${(props) =>
+    colorPicker(props.isFinish, props.isStart, props.isVisited, props.isWall)};
+`;
+
+const colorPicker = (
+  isFinish: boolean,
+  isStart: boolean,
+  isVisited: boolean,
+  isWall: boolean
+) => {
+  if (isFinish) {
+    return "red";
+  }
+  if (isStart) {
+    return "green";
+  }
+  if (isWall) {
+    return "brown";
+  }
+  if (isVisited) {
+    return "yellow";
+  }
+  return "white";
+};
 
 function App() {
 
@@ -41,34 +64,46 @@ function App() {
   console.log(grid)
 
   const changeColor = (id: number, x: number, y: number) => {
-    const dupa = [...grid]
-    dupa[id].isWall = !dupa[id].isWall
-    console.log(dupa[id].isWall)
-    setGrid([...dupa])
+    const grid_copy = [...grid]
+    grid_copy[id].isWall = !grid_copy[id].isWall
+    console.log(grid_copy[id].isWall)
+    setGrid([...grid_copy])
 
   }
 
+  const setStart = (id: number) => {
+    const grid_copy = grid;
+    grid_copy[id].isStart = true;
+    setGrid([...grid_copy]);
+  };
 
+  const setStop = (id: number) => {
+    const grid_copy = grid;
+    grid_copy[id].isFinish = true;
+    setGrid([...grid_copy]);
+  }
 
   return (
     <div>
       <Container>
         {
           grid.map((each: any, index: any) => (
-            <Wrapper 
-              key={index} 
-              onClick={() => changeColor(index, each.x, each.y)} 
-              id={index}
-              isWall={each.isWall}
+            <Wrapper
+            key={index}
+            onClick={() => changeColor(index, each.x, each.y)}
+            id={index}
+            isWall={each.isWall}
+            isFinish={each.isFinish}
+            isStart={each.isStart}
+            isVisited={each.isVisited}
             >
               {each.x}, {each.y}
             </Wrapper>
           ))
         }
       </Container>
-      <button onClick={() => {console.log('dupa')}}>
-        dupa
-      </button>
+      <button onClick={() => setStart(82)}>Select start</button>
+      <button onClick={() => setStop(97)}>Select end</button>
     </div>
 
   );
